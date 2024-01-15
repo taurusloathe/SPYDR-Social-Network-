6934825da2da46e1ca0c644ad5bb0236b5dd450b
taurusloathe <110080228+taurusloathe@users.noreply.github.com>
2024-01-15 17:43:44 -0500

    Update and rename signupbknd.js to signupmngodbbknd.js

---
+++ signupmngodbbknd.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/spydr-social', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a User schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  phoneNumber: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to SPYDR Social Signup Page!');
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/signup', async (req, res) => {
+  const { email, password, phoneNumber } = req.body;
+
+  try {
+    // Create a new user
+    const newUser = new User({ email, password, phoneNumber });
+    
+    // Save the user to the database
+    await newUser.save();
+    
+    res.send('Signup successful!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error during signup');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
